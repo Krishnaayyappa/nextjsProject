c40b30d34cfd33a332cf83eb82a9dd43776e9ce8
Hari Krishna Ayyappa Narina <krishnanarina8@gmail.com>
2023-07-10 00:14:30 -0400

    fallback

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -27,7 +27,7 @@ export async function getStaticPaths(){
      client.close();
 
      return {
-        fallback:false,
+        fallback:"blocking",
         paths: meetups.map((meetup) => ({
             params:{meetupid:meetup._id.toString()},
         }))   
